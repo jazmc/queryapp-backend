4b8d302ebe46983270f29c5114d1a792d5ec48b5
jazmc <jasmin.lumme@gmail.com>
2021-11-09 13:47:38 +0200

    Initial commit

---
+++ src/main/java/com/example/queryappbackend/QueryappBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.queryappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryappBackendApplication.class, args);
+		
+		System.out.println("Käynnistyy");
+	}
+
+}
---
+++ src/test/java/com/example/queryappbackend/QueryappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.queryappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
