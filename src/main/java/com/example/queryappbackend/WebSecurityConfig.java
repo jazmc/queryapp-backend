cd08b2002f801a77fd25a85ba1ecb0dd6cb3029d
jazmc <jasmin.lumme@gmail.com>
2021-11-23 17:15:39 +0200

    CORS to web security config

--- src/main/java/com/example/queryappbackend/WebSecurityConfig.java
+++ src/main/java/com/example/queryappbackend/WebSecurityConfig.java
@@ -15,12 +15,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-        .authorizeRequests().antMatchers("/**").permitAll()
+		.authorizeRequests().antMatchers("/**").permitAll()
         	.and()
         .authorizeRequests().anyRequest().authenticated()
           .and()
 			.logout()
 			.permitAll();
+		http.cors();
 	}
 	
 }
